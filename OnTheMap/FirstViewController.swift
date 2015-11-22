d4e1da1b5d27753ff6b62ecd4b6f550ed824828f
Craig Vanderzwaag <craig@bluehulastudios.com>
2015-11-21 23:25:35 -0800

    Initial Commit

---
+++ OnTheMap/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  OnTheMap
+//
+//  Created by Craig Vanderzwaag on 11/21/15.
+//  Copyright © 2015 blueHula Studios. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
